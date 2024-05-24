875cc618c131ead027c937529f3b54931f1327a0
Abomo Nguissimbi <21217284+abomo@users.noreply.github.com>
2024-05-24 08:42:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,36 +14,38 @@ const taskData = [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-      id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-      title: titleInput.value,
-      date: dateInput.value,
-      description: descriptionInput.value,
-    };
-  
-     if (dataArrIndex === -1) {
-      taskData.unshift(taskObj);
-    };
-    updateTaskContainer();
-    reset();
-}
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+  updateTaskContainer()
+  reset()
+};
 
 const updateTaskContainer = () => {
-    taskData.forEach(
-        ({ id, title, date, description }) => {
-            (tasksContainer.innerHTML += `
-            <div class="task" id="${id}">
-              <p><strong>Title:</strong> ${title}</p>
-              <p><strong>Date:</strong> ${date}</p>
-              <p><strong>Description:</strong> ${description}</p>
-              <button type="button" class="btn">Edit</button>
-              <button type="button" class="btn">Delete</button>
-            </div>
-          `)
-        }
-      );
-}
+  tasksContainer.innerHTML = "";
+
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        (tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button> 
+        </div>
+      `)
+    }
+  );
+};
 
 const reset = () => {
   titleInput.value = "";
@@ -59,10 +61,11 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
   const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
-  if(formInputsContainValues){confirmCloseDialog.showModal();}
-  else {reset();}
-
-  confirmCloseDialog.showModal();
+  if (formInputsContainValues) {
+    confirmCloseDialog.showModal();
+  } else {
+    reset();
+  }
 });
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
